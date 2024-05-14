4c811cc269d3370113a71931c95b0c751a7f9545
Alexandre  Shinebourne <43988572+a-shine@users.noreply.github.com>
2024-05-14 08:42:45 +0100

    changing to correct env

--- scripts/training-job.sh
+++ scripts/training-job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=orchid
 #SBATCH --account=orchid
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 
-source activate gtc_env
+source activate niab
 
 srun python scripts/train_plant_seg.py
