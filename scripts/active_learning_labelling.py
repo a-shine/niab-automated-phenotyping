5cc9790ebdddd8cedb0b6cf651ceb3302d580598
Alexandre  Shinebourne <43988572+a-shine@users.noreply.github.com>
2024-05-14 17:16:51 +0100

    uncertainty in segmentation

--- scripts/active_learning_labelling.py
+++ scripts/active_learning_labelling.py
@@ -1,26 +1,19 @@
-import sys
-import os
+# Batch script that runes inference with uncertainty on the images in the 
+# dataset.
 
-# Add the parent directory of current directory to the sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from utils.image_utils import load_image_ts, reduce_resolution, white_balance
 from utils.dl.niab import IMG_TRANSFORMS, ActiveLearningDataset
 import torch
-import segmentation_models_pytorch as smp
-from torch import nn
 from utils.dl.model import MCDUNet
-from typing import List
 
-dataset = ActiveLearningDataset("./datasets/niab/EXP01/Top_Images/Top_Images_Clean_Rename", IMG_TRANSFORMS)
+dataset = ActiveLearningDataset("/home/users/ashine/gws/niab-automated-phenotyping/datasets/niab/EXP01/Top_Images/Top_Images_Clean_Rename", IMG_TRANSFORMS)
 
-MODEL_PATH = "./models/best_model.pth"
+MODEL_PATH = "/home/users/ashine/gws/niab-automated-phenotyping/models/20240514122226/best_model.pth"
 
 device = ("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Running model using {device} device")
 
 # TODO: let's see if there is any variance (if none then the model might not have dropout layers so we need to add them)
-def predict_with_uncertainty(model, image, n_times=10):
+def predict_with_uncertainty(model, image, n_times=200):
     # Set model to training mode to enable dropout at test time
     model.train()
 
@@ -41,17 +34,11 @@ def predict_with_uncertainty(model, image, n_times=10):
 
     return mean, variance
 
-
-# # Create an instance of the model and move it to the device (GPU or CPU) and load the model parameters
-unet_dims: List = []
-for i in range(5):
-    unet_dims.append(2**(5 + i))
-
 # Create an instance of the model and move it to the device (GPU or CPU)
 model = MCDUNet(n_channels=3,
              n_classes=1,
              bilinear=True,
-             ddims=unet_dims,
+             ddims=[32, 64, 128, 256, 512],
              UQ=True,
              ).to(device)
 
@@ -61,31 +48,36 @@ model.load_state_dict(torch.load(MODEL_PATH))
 # Test adding dropout with the aux_ parameters (just train trhe model for 5 epochs and test with this script to see if there are any uncertainties i.e. that the model is non-deterministic)
 
 # create a csv with two columns: image_name, uncertainty
-csv = open("uncertainty.csv", "w")
-csv.write("image_name,uncertainty\n")
+csv = open("/home/users/ashine/gws/niab-automated-phenotyping/uncertainty.csv", "w")
+csv.write("img_path,mean_uncertainty,mcd_uncertainty\n")
 
 # Loop through the dataset and run prediction with uncertainty
 for name, image in dataset:
     m, u = predict_with_uncertainty(model, image)
 
-    # Calculate overall uncertainty
-    overall_uncertainty = torch.mean(u)
+    # Calculate mean uncertainty per pixel (useful if you want to know how uncertain the model is on average for each pixel)
+    # TODO: how could the mean uncertay be greater than 1? areneth the predction values all betwene 0 and 1
+    mean_uncertainty = torch.mean(u)
+    
+    # The original definition of MCD uncertainty involves normalizing by the volume 
+    # of the predicted mask, not the total number of pixels in the image. If the 
+    # predicted mask only covers a portion of the image, then the MCD uncertainty 
+    # would be a measure of average uncertainty within the mask, not the entire 
+    # image.
 
-    print(f"Image name: {name}, Overall uncertainty: {overall_uncertainty.item()}")
+    # If you want to calculate MCD uncertainty, you would need to sum and normalize
+    # only over the pixels within the predicted mask. This would give you a 
+    # different measure of uncertainty that might be more relevant if you're 
+    # specifically interested in the areas of the image where the model is making 
+    # predictions.
+    
+    # Calculate MCD Uncertainty
+    # global score of how certain or uncertain the model is given an input image, which can be useful if you want to get a single uncertainty value for the entire image
+    # mcd_uncertainty = torch.sum(u) / torch.numel(u)
+    # Calculate MCD Uncertainty within the predicted mask
+    mask = m > 0.5  # replace 'threshold' with appropriate value
+    mcd_uncertainty = torch.sum(u[mask]) / torch.sum(mask)
 
-    csv.write(f"{name},{overall_uncertainty.item()}\n")
+    print(f"Image name: {name}, Mean uncertainty: {mean_uncertainty.item()}, MCD Uncertainty: {mcd_uncertainty.item()}")
 
-
-# get the first image and run prediction with uncertainty
-# name, image = dataset[0]
-
-# print(f"Image name: {name}")
-
-# m, u = predict_with_uncertainty(model, image)
-
-# # TODO: plot an uncertainty mask/map (would be good to do cause it will tell me where the model struggles and also a good sanity check to know that my uncertainty values aren't noncense)
-
-# # Calculate overall uncertainty
-# overall_uncertainty = torch.mean(u)
-
-# print(f"Overall uncertainty: {overall_uncertainty.item()}")
+    csv.write(f"{name},{mean_uncertainty.item()},{mcd_uncertainty.item()}\n")
