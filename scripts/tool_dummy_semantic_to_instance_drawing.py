97e53029f615a3b4df8b9b9881f1b97114b89a5e
Alexandre Shinebourne <43988572+a-shine@users.noreply.github.com>
2024-05-30 12:08:14 +0100

    better tool

--- scripts/tool_dummy_semantic_to_instance_drawing.py
+++ scripts/tool_dummy_semantic_to_instance_drawing.py
@@ -4,13 +4,13 @@ import os
 
 # Define the drawing state and color
 drawing = False
-class_id = 0
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
+class_id = 1
 ix, iy = -1, -1
 min_line_width = 2
 max_line_width = 4
 output_dir = 'output'
 img_count = 0
+auto_increment = True 
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_dir):
@@ -20,7 +20,7 @@ if not os.path.exists(output_dir):
 if not os.path.exists(os.path.join(output_dir, 'binary_masks')):
     os.makedirs(os.path.join(output_dir, 'binary_masks'))
 
-    # create instance_masks directory
+# create instance_masks directory
 if not os.path.exists(os.path.join(output_dir, 'instance_masks')):
     os.makedirs(os.path.join(output_dir, 'instance_masks'))
 
@@ -29,39 +29,50 @@ binary_mask = np.zeros((345, 460), np.uint8)
 
 # Mouse callback function
 def draw_line(event, x, y, flags, param):
-    global drawing, img, binary_mask, class_id, ix, iy
+    global drawing, instance_mask, binary_mask, class_id, ix, iy
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix, iy = x, y
+        if auto_increment:
+            class_id += 1
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
             line_width = np.random.randint(min_line_width, max_line_width)
-            cv2.line(img, (ix, iy), (x, y), colors[class_id], line_width)
+            cv2.line(instance_mask, (ix, iy), (x, y), class_id, line_width)
             cv2.line(binary_mask, (ix, iy), (x, y), 255, line_width)
             ix, iy = x, y
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         line_width = np.random.randint(min_line_width, max_line_width)
-        cv2.line(img, (ix, iy), (x, y), colors[class_id], line_width)
+        cv2.line(instance_mask, (ix, iy), (x, y), class_id, line_width)
         cv2.line(binary_mask, (ix, iy), (x, y), 255, line_width)
 
 # Create a black image and a window
-img = np.zeros((345, 460, 3), np.uint8)
+instance_mask = np.zeros((345, 460), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_line)
 
 while(1):
-    cv2.imshow('image', img)
+    # Normalize the grayscale values to the full range of 0-255
+    img_normalized = cv2.normalize(instance_mask, None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U)
+
+    # Apply the rainbow colormap to the normalized image
+    display_image = cv2.applyColorMap(img_normalized, cv2.COLORMAP_JET)
+    cv2.imshow('image', display_image)
     k = cv2.waitKey(1) & 0xFF
-    if k == ord('n'):  # Press 'n' to change the class
-        class_id = (class_id + 1) % len(colors)
-    elif k == ord('t'):  # Press 't' to save the current image and create a new one
-        cv2.imwrite(os.path.join(output_dir, f'instance_masks/{img_count}.jpg'), img)
-        cv2.imwrite(os.path.join(output_dir, f'binary_masks/{img_count}.jpg'), binary_mask)
-        img = np.zeros((345, 460, 3), np.uint8)
+    if k == ord('i'):  # Press 'n' to change the class
+        class_id += 1
+    elif k == ord('a'):  # Press 'a' to toggle auto increment
+        auto_increment = not auto_increment
+        print(f'Auto increment: {auto_increment}')
+    elif k == ord('n'):  # Press 't' to save the current image and create a new one
+        class_id = 1
+        cv2.imwrite(os.path.join(output_dir, f'binary_masks/{img_count}.png'), binary_mask)
+        cv2.imwrite(os.path.join(output_dir, f'instance_masks/{img_count}.png'), instance_mask)
+        instance_mask = np.zeros((345, 460), np.uint8)
         binary_mask = np.zeros((345, 460), np.uint8)
         img_count += 1
     elif k == 27:  # Press 'ESC' to exit
