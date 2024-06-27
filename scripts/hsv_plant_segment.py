e35944345f9a6def27476c62bc9110a9f1fd3410
Alexandre  Shinebourne <43988572+a-shine@users.noreply.github.com>
2024-06-27 17:23:09 +0100

    making plant segment util package

--- scripts/hsv_plant_segment.py
+++ scripts/hsv_plant_segment.py
@@ -22,32 +22,11 @@ import glob
 import os
 
 import cv2
-import numpy as np
 
-from utils.image_utils import white_balance as wb
+from utils.hsv_plant_segment import segment_plants
 
 HSVMIN_WB = (30, 80, 0)
 HSVMAX_WB = (65, 255, 255)
-
-
-def segment_plants(img):
-    lower_wb = np.array(HSVMIN_WB)
-    upper_wb = np.array(HSVMAX_WB)
-
-    # Create HSV Image from white balanced image and threshold into a range.
-    wb_img = wb(img)
-    hsv_img = cv2.cvtColor(wb_img, cv2.COLOR_BGR2HSV)
-    mask_wb = cv2.inRange(hsv_img, lower_wb, upper_wb)
-
-    # Combine the masks
-    # mask = cv2.bitwise_or(mask_raw, mask_wb)
-
-    # Clean up the mask
-    kernel = np.ones((3, 3), np.uint8)
-    opening = cv2.morphologyEx(mask_wb, cv2.MORPH_OPEN, kernel)
-    output = cv2.dilate(opening, kernel, iterations=10)
-
-    return wb_img, output
 
 
 if __name__ == "__main__":
@@ -68,7 +47,7 @@ if __name__ == "__main__":
     # Loop through all the images in th
     for i in range(len(images)):
         img = cv2.imread(images[i])
-        wb_img, mask = segment_plants(img)
+        wb_img, mask = segment_plants(img, HSVMIN_WB, HSVMAX_WB)
 
         # Save the images and masks
         cv2.imwrite("./output/imgs/" + os.path.basename(images[i]), wb_img)
--- utils/hsv_plant_segment.py
+++ utils/hsv_plant_segment.py
@@ -3,29 +3,15 @@ import numpy as np
 
 import utils.image_utils as wb
 
-HSVMIN_RAW = (30, 170, 0)
-HSVMAX_RAW = (65, 255, 255)
-HSVMIN_WB = (30, 80, 0)
-HSVMAX_WB = (65, 255, 255)
 
-
-def segment_plants(img):
-    # lower_raw = np.array(HSVMIN_RAW)
-    # upper_raw = np.array(HSVMAX_RAW)
-    lower_wb = np.array(HSVMIN_WB)
-    upper_wb = np.array(HSVMAX_WB)
-
-    # Create HSV Image and threshold into a range.
-    # hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # mask_raw = cv2.inRange(hsv_img, lower_raw, upper_raw)
+def segment_plants(img, hsvmin_wb, hsvmax_wb):
+    lower_wb = np.array(hsvmin_wb)
+    upper_wb = np.array(hsvmax_wb)
 
     # Create HSV Image from white balanced image and threshold into a range.
-    wb_img = wb.white_balance(img)
+    wb_img = wb(img)
     hsv_img = cv2.cvtColor(wb_img, cv2.COLOR_BGR2HSV)
     mask_wb = cv2.inRange(hsv_img, lower_wb, upper_wb)
-
-    # Combine the masks
-    # mask = cv2.bitwise_or(mask_raw, mask_wb)
 
     # Clean up the mask
     kernel = np.ones((3, 3), np.uint8)
